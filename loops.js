9dad777239f6e69aa6a57b7fb23705efecbbe105
sunaina rishi <sunaina.d.rishi@gmail.com>
2023-04-25 08:46:02 +0100

    Loops and Functions as values

--- loops.js
+++ loops.js
@@ -10,6 +10,17 @@ for (let i = 1 ; i <=20 ; i++) {
   }
 };
 
+/* Another syntax for loops using while
+
+let i = 0
+
+while (i <= 10) {
+  console.log(i);
+
+  i += 1;
+} 
+*/
+
 // Challenge: Write a function fizzbuzzUntil that accepts a number. 
 // This function should use a loop to loop through all numbers from 1 to the given one, 
 // call the fizzBuzz function for the current number and print the result.
